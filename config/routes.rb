12cd8086729fb112157ad22930b5e70902ee487f
ljw3728@gmail.com <ljw3728@gmail.com>
2019-01-14 19:59:00 -0500

    temp route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  #temp
+  root 'users#index'
+
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
